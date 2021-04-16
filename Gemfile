f85fbc7cfd85b8f79ff675e48fd6aa4e252cf873
Payton Green <paytonagreen@gmail.com>
2021-04-16 16:07:46 -0500

    devise setup

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'bootstrap', '~> 4.6'
 gem 'jquery-rails'
 gem "stock_quote", '~> 3.0.0'
+gem 'devise'
 
 
 group :development, :test do
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'sk_3e37f43ac77b4548a61e1b623b0478dc' )
-    if params[:ticker]
-      @stock = StockQuote::Stock.quote(params[:ticker])
+    if params[:ticker] == ''
+      @nothing = "Hey, you forgot to enter a symbol!!"
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue
+        @stock = nil
+      end
+      if !@stock
+        @error = "Hey, that symbol doesn't seem to exist! Try again!"
+      end
     end
   end
 
